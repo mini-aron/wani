b0e180860400216863b8b94e77acdab52bfabb5e
우엥 <105274015+rnldudns@users.noreply.github.com>
2023-03-08 22:47:30 +0900

    netlify test

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/webtoon" element={<Webtoon/>}/>
       
       </Routes>
